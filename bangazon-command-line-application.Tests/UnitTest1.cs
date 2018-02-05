dc75cc9fd246b58a2c83bd5deab50ea983ff17bf
krysmathis <krysmathis@gmail.com>
2018-02-05 14:30:11 -0600

    initial commit

---
+++ bangazon-command-line-application.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace bangazon_command_line_application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ bangazon-command-line-application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bangazon_command_line_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
