fcfcad206bf599e57a9ce9ad39989b2dbe5f63ed
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-02-07 10:36:05 -0600

    working on update product

--- bangazon-cli/Managers/ProductManager.cs
+++ bangazon-cli/Managers/ProductManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using bangazon_cli.Models;
+using System.Linq;
 
 namespace bangazon_cli.Managers
 {
@@ -25,6 +26,26 @@ namespace bangazon_cli.Managers
         public List<Product> GetProducts()
         {
             return _products;
+        }
+
+        /*
+            Updates product property in database. 
+            Parameters: 
+            - Product Object
+            - String property 
+            - Key
+        */
+        public void Update (Product product, string property, int key)
+        {
+            System.Collections.Hashtable h = new System.Collections.Hashtable();
+            h[product.key] = property;
+            // product.Name = name;
+        }
+
+        // gets one product. Parameters: id
+        public Product GetSingleProduct (int id)
+        {
+            return _products.Where(p => p.Id == id).Single();
         }
 
     }
