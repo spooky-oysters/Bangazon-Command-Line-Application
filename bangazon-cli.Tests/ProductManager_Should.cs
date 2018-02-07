521621e48e615d0e3d2587763e13ac53f2fc65fe
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-02-07 11:43:45 -0600

    update product price and quantity working

--- bangazon-cli.Tests/ProductManager_Should.cs
+++ bangazon-cli.Tests/ProductManager_Should.cs
@@ -6,7 +6,7 @@ namespace bangazon_cli.Managers.Tests
 {
     /*
         Author: Kimberly Bird
-        References #4 - Create Product
+        References #4 - Create Product, #8 - Update Product Properties
     */
     public class ProductManager_Should
     {
@@ -53,7 +53,7 @@ namespace bangazon_cli.Managers.Tests
         [Fact]
         public void UpdateProductDesc()
         {
-            // add new product with product name
+            // add new product with product description
             _product = new Models.Product();
             _product.Id = 1;
             _product.Description = "Kite description";
@@ -68,12 +68,61 @@ namespace bangazon_cli.Managers.Tests
             // select one product to update based on id
             Product prodToUpdate = productManager.GetSingleProduct(1);
 
-            // updates product name 
+            // updates product description 
             productManager.UpdateDescription(prodToUpdate, "New Kite description");
 
-            // tests that new product name is updated
+            // tests that new product description is updated
             Assert.Equal(prodToUpdate.Description, "New Kite description");
         }
 
+        [Fact]
+        public void UpdateProductPrice()
+        {
+            // add new product with product price
+            _product = new Models.Product();
+            _product.Id = 1;
+            _product.Price = 45.00;
+
+            // new product manager instance 
+            ProductManager productManager = new ProductManager();
+            productManager.AddProduct(_product);
+
+            // list all products
+            var _listProducts = productManager.GetProducts();
+
+            // select one product to update based on id
+            Product prodToUpdate = productManager.GetSingleProduct(1);
+
+            // updates product price 
+            productManager.UpdatePrice(prodToUpdate, 50.97);
+
+            // tests that new product price is updated
+            Assert.Equal(prodToUpdate.Price, 50.97);
+        }
+
+        [Fact]
+        public void UpdateProductQuantity()
+        {
+            // add new product with product quantity
+            _product = new Models.Product();
+            _product.Id = 1;
+            _product.Quantity = 4;
+
+            // new product manager instance 
+            ProductManager productManager = new ProductManager();
+            productManager.AddProduct(_product);
+
+            // list all products
+            var _listProducts = productManager.GetProducts();
+
+            // select one product to update based on id
+            Product prodToUpdate = productManager.GetSingleProduct(1);
+
+            // updates product quantity 
+            productManager.UpdateQuantity(prodToUpdate, 5);
+
+            // tests that new product quantity is updated
+            Assert.Equal(prodToUpdate.Quantity, 5);
+        }
     }
 }
