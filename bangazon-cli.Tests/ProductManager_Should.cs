d1b014d8504b7e9e5395160730b5cce42938f821
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-02-07 11:06:17 -0600

    working on product

--- bangazon-cli.Tests/ProductManager_Should.cs
+++ bangazon-cli.Tests/ProductManager_Should.cs
@@ -26,7 +26,7 @@ namespace bangazon_cli.Managers.Tests
         }
 
         [Fact]
-        public void UpdateProduct()
+        public void UpdateProductName()
         {
             // add new product with product name
             _product = new Models.Product();
@@ -44,8 +44,33 @@ namespace bangazon_cli.Managers.Tests
             Product prodToUpdate = productManager.GetSingleProduct(1);
 
             // updates product name 
-            productManager.Update(prodToUpdate, "New Kite");
-            
+            productManager.UpdateName(prodToUpdate, "New Kite");
+
+            // tests that new product name is updated
+            Assert.Equal(prodToUpdate.Name, "New Kite");
+        }
+
+        [Fact]
+        public void UpdateProductDesc()
+        {
+            // add new product with product name
+            _product = new Models.Product();
+            _product.Id = 1;
+            _product.Description = "Kite description";
+
+            // new product manager instance 
+            ProductManager productManager = new ProductManager();
+            productManager.AddProduct(_product);
+
+            // list all products
+            var _listProducts = productManager.GetProducts();
+
+            // select one product to update based on id
+            Product prodToUpdate = productManager.GetSingleProduct(1);
+
+            // updates product name 
+            productManager.UpdateDescription(prodToUpdate, "New Kite description");
+
             // tests that new product name is updated
             Assert.Equal(prodToUpdate.Name, "New Kite");
         }
