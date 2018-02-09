51d116c3b819947f8a3cfd4b283216ebce680ba0
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-02-09 11:43:37 -0600

    add product menu working

--- bangazon-cli/Program.cs
+++ bangazon-cli/Program.cs
@@ -12,7 +12,7 @@ namespace bangazon_cli
             // Initalize the interface
             string prodPath = System.Environment.GetEnvironmentVariable("BANGAZON_CLI_APP_DB");
             DatabaseInterface db = new DatabaseInterface(prodPath);
-       
+
             // Initialize the Manager objects
             CustomerManager customerManager = new CustomerManager(db);
             ActiveCustomerManager activeCustomerManager = new ActiveCustomerManager(customerManager);
@@ -22,45 +22,62 @@ namespace bangazon_cli
 
             int choice;
             // When the user enters the system show the main menu
-            do {
+            do
+            {
                 choice = MainMenu.Show(activeCustomer);
 
-                switch (choice) {
-                    
+                switch (choice)
+                {
+
                     // Add customer
-                    case 1: {
-                        AddCustomerMenu customerMenu = new AddCustomerMenu(new Customer(), customerManager);
-                        customerMenu.Show();
-                        break;
-                    }
+                    case 1:
+                        {
+                            AddCustomerMenu customerMenu = new AddCustomerMenu(new Customer(), customerManager);
+                            customerMenu.Show();
+                            break;
+                        }
 
                     /*
                         List the customers and allow the user to select a customer based on the
                         position in the list
                     */
-                    case 2: {
-                        ActiveCustomerMenu activeCustomerMenu = new ActiveCustomerMenu(customerManager);
-                        int customerId = activeCustomerMenu.Show();
-                        activeCustomer = activeCustomerManager.SetActiveCustomer(customerId);
-                        break;
-                    }
+                    case 2:
+                        {
+                            ActiveCustomerMenu activeCustomerMenu = new ActiveCustomerMenu(customerManager);
+                            int customerId = activeCustomerMenu.Show();
+                            activeCustomer = activeCustomerManager.SetActiveCustomer(customerId);
+                            break;
+                        }
+
+                    /*
+                        Add product to active customer 
+                     */
+                    case 4:
+                        {
+                            AddProductMenu addProductMenu = new AddProductMenu(activeCustomer, new Product(), productManager);
+                            addProductMenu.Show();
+                            break;
+
+                        }
 
                     /*
                         Lists all products to allow user to choose one to add to their order. When product is chosen, the product is added to the active customer's order
                     */
-                    case 6: {
-                        AddProductToCartMenu addProductToCartMenu = new AddProductToCartMenu(activeCustomer, orderManager, productManager);
-                        addProductToCartMenu.Show();
-                        break;
-                    }
+                    case 6:
+                        {
+                            AddProductToCartMenu addProductToCartMenu = new AddProductToCartMenu(activeCustomer, orderManager, productManager);
+                            addProductToCartMenu.Show();
+                            break;
+                        }
 
-                    default: {
-                        break;
-                    }
+                    default:
+                        {
+                            break;
+                        }
                 }
 
             } while (choice != 9);
 
-      }
+        }
     }
 }
