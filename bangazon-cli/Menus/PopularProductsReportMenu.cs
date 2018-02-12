cf523ce5da93eec2f6ccd057eb1bd588afba1da7
Dre mf Randaci <drerandaci@gmail.com>
2018-02-12 16:29:37 -0600

    query for the report menu

--- bangazon-cli/Menus/PopularProductsReportMenu.cs
+++ bangazon-cli/Menus/PopularProductsReportMenu.cs
@@ -17,34 +17,65 @@ namespace bangazon_cli.Menus
 
             _popularProducts = new List<PopularProductsReport>();
 
-            var item1 = new PopularProductsReport("Kite1", "5000", "2000", "1920.20");
-            var item2 = new PopularProductsReport("Kite2", "500", "200", "920.20");
-            var item3 = new PopularProductsReport("Kite3", "50", "20", "90.20");
+            _db.Query($@"select *
+                from
+                (
+                select p.Name, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
+                from Product p
+                join OrderProduct op on p.Id = op.ProductId
+                join (select p.Id ProductId, count(distinct o.CustomerId) Purchasers
+                from Product p
+                join OrderProduct on p.Id = OrderProduct.ProductId
+                join `Order` o on OrderProduct.OrderId = o.Id
+                group by p.Name) CustomerCount on p.Id = CustomerCount.ProductId
+                group by p.Name 
+                order by Revenue desc
+                limit 3
+                )
 
-            _popularProducts.Add(item1);
-            _popularProducts.Add(item2);
-            _popularProducts.Add(item3);
-
-            // _db.Query($@"SELECT * FROM Product;", (SqliteDataReader reader) =>
-            // {
-            //     while (reader.Read())
-            //     {
-
-            //     }
-            // });   
+                union all
+                select ""Totals"" Totals, sum(""Orders"") Orders, sum(""Purchasers"") Purchasers, sum(""Revenue"") Revenue
+                from (
+                select p.Name Product, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
+                from Product p
+                join OrderProduct op on p.Id = op.ProductId
+                join (select p.Id ProductId, count(distinct o.CustomerId) Purchasers
+                from Product p
+                join OrderProduct on p.Id = OrderProduct.ProductId
+                join `Order` o on OrderProduct.OrderId = o.Id
+                group by p.Name) CustomerCount on p.Id = CustomerCount.ProductId
+                group by p.Name 
+                order by Revenue desc
+                limit 3
+                );", (SqliteDataReader reader) =>
+            {
+                while (reader.Read() && _popularProducts.Count <= 3)
+                {
+                    PopularProductsReport report = new PopularProductsReport();
+                    report.Product = Convert.ToString(reader["Product"]);
+                    report.Orders = Convert.ToString(reader["Orders"]);
+                    report.Purchasers = Convert.ToString(reader["Purchasers"]);
+                    report.Revenue = Convert.ToString(reader["Revenue"]);
+                    _popularProducts.Add(report);
+                }
+            });   
         }
         
         
         public void Show()
         {
             string keyPress;
-            
             do
             {
                 Console.Clear();
                 Console.WriteLine($"{"Product",-20}{"Orders",-11}{"Purchasers",-15}{"Revenue",-15}");
                 Console.WriteLine(new string('*', 61));
-                Console.WriteLine("");
+                foreach (var row in _popularProducts)
+                {
+                    Console.WriteLine($"{row.Product,-18}{row.Orders,-11}{row.Purchasers,-15}${row.Revenue,-15}");
+                }
+                Console.WriteLine(new string('*', 61));
+                Console.WriteLine($"{"Totals",-20}{"",-11}{"",-15}{"",-15}");
                 keyPress = Console.ReadLine();
             } while (keyPress != "");
             
--- bangazon-cli/Models/PopularProductsReport.cs
+++ bangazon-cli/Models/PopularProductsReport.cs
@@ -2,18 +2,9 @@ namespace bangazon_cli.Models
 {
     public class PopularProductsReport
     {
-        private string Product { get; set; }
-        private string Orders { get; set; }
-        private string Purchasers { get; set; }
-        private string Revenue { get; set; }
-
-        public PopularProductsReport(string product, string order, string purchasers, string revenue)
-        {
-            this.Product = product;
-            this.Orders = order;
-            this.Purchasers = purchasers;
-            this.Revenue = revenue;
-        }
-
+        public string Product { get; set; }
+        public string Orders { get; set; }
+        public string Purchasers { get; set; }
+        public string Revenue { get; set; }
     }
 }
