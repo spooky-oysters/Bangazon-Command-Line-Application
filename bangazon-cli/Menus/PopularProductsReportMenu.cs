4c9b3b8000ea78b2c8a60fc2cb521addd186fadb
Dre mf Randaci <drerandaci@gmail.com>
2018-02-12 20:11:03 -0600

    report formatted and data returning

--- bangazon-cli/Menus/PopularProductsReportMenu.cs
+++ bangazon-cli/Menus/PopularProductsReportMenu.cs
@@ -17,39 +17,28 @@ namespace bangazon_cli.Menus
 
             _popularProducts = new List<PopularProductsReport>();
 
-            _db.Query($@"select *
-                from
-                (
-                select p.Name, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
+            _db.Query($@"
+            with PopularItems as
+                (select p.Name Product, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
                 from Product p
                 join OrderProduct op on p.Id = op.ProductId
-                join (select p.Id ProductId, count(distinct o.CustomerId) Purchasers
+                join 
+                (select p.Id ProductId, count(distinct o.CustomerId) Purchasers
                 from Product p
                 join OrderProduct on p.Id = OrderProduct.ProductId
                 join `Order` o on OrderProduct.OrderId = o.Id
-                group by p.Name) CustomerCount on p.Id = CustomerCount.ProductId
+                group by p.Name) 
+                CustomerCount on p.Id = CustomerCount.ProductId
                 group by p.Name 
                 order by Revenue desc
                 limit 3
                 )
-
+                select * from PopularItems
                 union all
                 select ""Totals"" Totals, sum(""Orders"") Orders, sum(""Purchasers"") Purchasers, sum(""Revenue"") Revenue
-                from (
-                select p.Name Product, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
-                from Product p
-                join OrderProduct op on p.Id = op.ProductId
-                join (select p.Id ProductId, count(distinct o.CustomerId) Purchasers
-                from Product p
-                join OrderProduct on p.Id = OrderProduct.ProductId
-                join `Order` o on OrderProduct.OrderId = o.Id
-                group by p.Name) CustomerCount on p.Id = CustomerCount.ProductId
-                group by p.Name 
-                order by Revenue desc
-                limit 3
-                );", (SqliteDataReader reader) =>
+                from PopularItems;", (SqliteDataReader reader) =>
             {
-                while (reader.Read() && _popularProducts.Count <= 3)
+                while (reader.Read())
                 {
                     PopularProductsReport report = new PopularProductsReport();
                     report.Product = Convert.ToString(reader["Product"]);
@@ -58,9 +47,8 @@ namespace bangazon_cli.Menus
                     report.Revenue = Convert.ToString(reader["Revenue"]);
                     _popularProducts.Add(report);
                 }
-            });   
+            });
         }
-        
         
         public void Show()
         {
@@ -70,12 +58,17 @@ namespace bangazon_cli.Menus
                 Console.Clear();
                 Console.WriteLine($"{"Product",-20}{"Orders",-11}{"Purchasers",-15}{"Revenue",-15}");
                 Console.WriteLine(new string('*', 61));
-                foreach (var row in _popularProducts)
+                for (int i = 0; i <= _popularProducts.Count; i++)
                 {
-                    Console.WriteLine($"{row.Product,-18}{row.Orders,-11}{row.Purchasers,-15}${row.Revenue,-15}");
+                    if (i != 3)
+                    {
+                        Console.WriteLine($"{_popularProducts[i].Product,-20}{_popularProducts[i].Orders,-11}{_popularProducts[i].Purchasers,-15}${_popularProducts[i].Revenue,-15}");
+                    } else {
+                        Console.WriteLine(new string('*', 61));
+                        Console.WriteLine($"{_popularProducts[i].Product,-20}{_popularProducts[i].Orders,-11}{_popularProducts[i].Purchasers,-15}${_popularProducts[i].Revenue,-15}");
+                        break;
+                    }
                 }
-                Console.WriteLine(new string('*', 61));
-                Console.WriteLine($"{"Totals",-20}{"",-11}{"",-15}{"",-15}");
                 keyPress = Console.ReadLine();
             } while (keyPress != "");
             
