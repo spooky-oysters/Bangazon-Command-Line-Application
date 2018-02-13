06f328b23555c6b2eac039c6b10d645dadcdbe3f
Dre mf Randaci <drerandaci@gmail.com>
2018-02-12 20:51:41 -0600

    code comments

--- bangazon-cli/Menus/PopularProductsReportMenu.cs
+++ bangazon-cli/Menus/PopularProductsReportMenu.cs
@@ -7,16 +7,22 @@ namespace bangazon_cli.Menus
 {
     public class PopularProductsReportMenu
     {
+        // Initializing a db to use for the SQL query
         private DatabaseInterface _db;
+
+        // List to hold all the return query items
         private List<PopularProductsReport> _popularProducts;
 
         public PopularProductsReportMenu()
         {
+            // Path to the local database file
             string dbPath = System.Environment.GetEnvironmentVariable("BANGAZON_CLI_APP_DB");
             _db = new DatabaseInterface(dbPath);
 
+            // New list instance to hold query results and loop over to print to the console
             _popularProducts = new List<PopularProductsReport>();
 
+            // SQL query. Creates a temporary table that is then sub-queried  
             _db.Query($@"
             with PopularItems as
                 (select p.Name Product, count(op.Id) as Orders, CustomerCount.Purchasers as Purchasers, sum(p.Price) as Revenue
@@ -40,11 +46,14 @@ namespace bangazon_cli.Menus
             {
                 while (reader.Read())
                 {
+                    // Creates an instance of a single PopularProductsReport class to hold each report that comes back from the database
                     PopularProductsReport report = new PopularProductsReport();
                     report.Product = Convert.ToString(reader["Product"]);
                     report.Orders = Convert.ToString(reader["Orders"]);
                     report.Purchasers = Convert.ToString(reader["Purchasers"]);
                     report.Revenue = Convert.ToString(reader["Revenue"]);
+
+                    // Adds each report to the list
                     _popularProducts.Add(report);
                 }
             });
@@ -52,12 +61,21 @@ namespace bangazon_cli.Menus
         
         public void Show()
         {
-            string keyPress;
+            // Listens for a keypress to return the user to the main menu
+            ConsoleKeyInfo enteredKey;
+
+            // Builds the console table to display the information
             do
             {
                 Console.Clear();
+
+                // The string args represent column headers
                 Console.WriteLine($"{"Product",-20}{"Orders",-11}{"Purchasers",-15}{"Revenue",-15}");
+
+                // Prints 61 asterisks
                 Console.WriteLine(new string('*', 61));
+
+                // Loops over the query list and once the counter is above 3 items, prints another row of asterisks to separate the final "Totals" list element
                 for (int i = 0; i <= _popularProducts.Count; i++)
                 {
                     if (i != 3)
@@ -65,13 +83,14 @@ namespace bangazon_cli.Menus
                         Console.WriteLine($"{_popularProducts[i].Product,-20}{_popularProducts[i].Orders,-11}{_popularProducts[i].Purchasers,-15}${_popularProducts[i].Revenue,-15}");
                     } else {
                         Console.WriteLine(new string('*', 61));
-                        Console.WriteLine($"{_popularProducts[i].Product,-20}{_popularProducts[i].Orders,-11}{_popularProducts[i].Purchasers,-15}${_popularProducts[i].Revenue,-15}");
+                        Console.WriteLine($"{_popularProducts[i].Product + ':',-20}{_popularProducts[i].Orders,-11}{_popularProducts[i].Purchasers,-15}${_popularProducts[i].Revenue,-15}");
                         break;
                     }
                 }
-                keyPress = Console.ReadLine();
-            } while (keyPress != "");
-            
+                Console.WriteLine("");
+                Console.WriteLine("-> Press any key to return to main menu");
+                enteredKey = Console.ReadKey();
+            } while (enteredKey.KeyChar.ToString() == "");
         }
     }
 }
